dde869a10fe23a655071f24f2eb8a26c6a1cdef1
julianloaiza <loaizajulian1999@gmail.com>
2022-04-21 20:23:50 -0500

    Creacion de entidades

---
+++ src/main/java/com/platzi/market/persistence/entity/ComprasProductoPK.java
@@ -0,0 +1,30 @@
+package com.platzi.market.persistence.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Embeddable
+public class ComprasProductoPK implements Serializable {
+    @Column(name = "id_compra")
+    private Integer id_compra;
+
+    @Column(name = "id_producto")
+    private Integer idProducto;
+
+    public Integer getId_compra() {
+        return id_compra;
+    }
+
+    public void setId_compra(Integer id_compra) {
+        this.id_compra = id_compra;
+    }
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+}
